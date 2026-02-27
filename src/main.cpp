69b6eb5f128a599f0842a43c43d3a494fd630b1c
mryshkov <m.ryshkov@proton.me>
2026-02-27 09:31:30 +0100

    added star

--- src/main.cpp
+++ src/main.cpp
@@ -9,22 +9,29 @@
 using namespace std;
 
 
-static void DrawTree(vector<vector<char>> tree, int x, int y);
+void DrawTree(vector<vector<char>> tree, int x, int y);
 
-static void DrawMap(vector<vector<char>> map, vector<vector<char>> tree, vector<vector<char>> tree_light, char memoryCell, int memX, int memY);
+void DrawStar(vector<vector<char>> star, int X, int Y);
+
+void DrawMap(vector<vector<char>> map, vector<vector<char>> tree, vector<vector<char>> tree_light, vector<vector<char>> star, char memoryCell, int memX, int memY);
 
 
 int main(){
     InitWindow(1024, 576, "Game");
     SetTargetFPS(60);
-    
     string line;
+    fstream file_map("assets/map.txt");
+    vector<vector<char>> map;
+    fstream file_tree("assets/tree.txt");
+    vector<vector<char>> tree;
+    fstream file_tree_light("assets/tree_light.txt");
+    vector<vector<char>> tree_light;
+    fstream file_star("assets/star.txt");
+    vector<vector<char>> star;
     int memX, memY;
     char player = 'p', memoryCell;
 
     // map reading
-    fstream file_map("assets/map.txt");
-    vector<vector<char>> map;
     while(getline(file_map, line)) {
         vector<char> row;
         for (char c : line) {
@@ -34,8 +41,6 @@ int main(){
     }
 
     // object reading
-    fstream file_tree("assets/tree.txt");
-    vector<vector<char>> tree;
     while(getline(file_tree, line)) {
         vector<char> row;
         for (char c : line) {
@@ -44,8 +49,6 @@ int main(){
         tree.push_back(row);
     }
 
-    fstream file_tree_light("assets/tree_light.txt");
-    vector<vector<char>> tree_light;
     while (getline(file_tree_light, line)) {
         vector<char> row;
         for (char c : line) {
@@ -54,8 +57,16 @@ int main(){
         tree_light.push_back(row);
     }
 
+    while (getline(file_star, line)){
+        vector<char> row;
+        for (char c : line){
+            row.push_back(c);
+        }
+        star.push_back(row);
+    }
+
     // error in reading 
-    if (!file_map.is_open() || !file_tree.is_open() || !file_tree_light.is_open()) {
+    if (!file_map.is_open() || !file_tree.is_open() || !file_tree_light.is_open() || !file_star.is_open()) {
         cout << "Unable to open file." << endl; 
         return -1;
     } 
@@ -71,7 +82,7 @@ int main(){
 
         // map drawing
         int x = 0, y = 0;
-        DrawMap(map, tree, tree_light, memoryCell, memX, memY);
+        DrawMap(map, tree, tree_light, star, memoryCell, memX, memY);
         
 
         // spawning tosya
@@ -124,7 +135,40 @@ void DrawTree(vector<vector<char>> tree, int X, int Y){
     }
 }
 
-void DrawMap(vector<vector<char>> map, vector<vector<char>> tree, vector<vector<char>> tree_light, char memoryCell, int memX, int memY){
+void DrawStar(vector<vector<char>> star, int X, int Y){
+    int x = X, y = Y;
+    for (const auto& row : star) {
+        x = X;
+        for (const auto& col : row) {
+            if (col == ' ') {
+                x += 2;
+            }
+            else if (col == '4') {
+                DrawRectangle(x, y, 2, 2, Color{254, 219, 72, 255});
+                x += 2;
+            }
+            else if (col == '2') {
+                DrawRectangle(x, y, 2, 2, Color{255, 176, 62, 255});
+                x += 2;
+            }
+            else if (col == '5') {
+                DrawRectangle(x, y, 2, 2, Color{254, 239, 76, 255});
+                x += 2;
+            }
+            else if (col == '1') {
+                DrawRectangle(x, y, 2, 2, Color{169, 116, 66, 255});
+                x += 2;
+            }
+            else if (col == '3') {
+                DrawRectangle(x, y, 2, 2, Color{254, 196, 68, 255});
+                x += 2;
+            }
+        }
+        y += 2;
+    }
+}
+
+void DrawMap(vector<vector<char>> map, vector<vector<char>> tree, vector<vector<char>> tree_light, vector<vector<char>> star, char memoryCell, int memX, int memY){
     int x = 0, y = 0;
 
     for (int i = 0; i < map.size(); i++) {
@@ -147,7 +191,7 @@ void DrawMap(vector<vector<char>> map, vector<vector<char>> tree, vector<vector<
                 x += 32;
             }
             else if (col == '*'){
-                DrawRectangle(x, y, TILE, GOLD);
+                DrawStar(star, x, y);
                 x += 32;
             }
             else if (col == 't') {
